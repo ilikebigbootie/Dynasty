5de36064950b352cdaaed588da622fd1602c4df8
ilikebigbootie <lazer.ekholm@yahoo.com>
2025-11-11 17:06:11 -0500

    Enable blog feature in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -3,7 +3,7 @@ export const leagueID = "1182871210424737792"; // your league ID
 export const leagueName = "Welcome to Dynasty Boys 😤"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
